6e0213f18b25cf78bd627905c173e2ef7d6493c6
caenar <73683836+notansjwmember@users.noreply.github.com>
2024-12-19 00:59:25 +0800

    huhu pwede bang mamatay nalang

--- raw/dynamic.py
+++ raw/dynamic.py
@@ -56,12 +56,21 @@ def knapsack_dp(weights, values, capacity):
     # Rows = items (0 to n), Columns = capacities (0 to capacity)
     # Add 1 to both dimensions to account for the "0-item" and "0-capacity" cases
     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
-    
+    print(dp)
+    print()
     # Start looping through the items (row by row)
+    # Though we start at row 1, because row 0 is our reference
     for i in range(1, n + 1):
         # Now loop through all possible capacities (column by column)
+        # Stop at capacity that is added by 1 to account for the zero base initialization
         for w in range(capacity + 1):
             # Check if the current item's weight can fit in the current capacity
+            # At the base, since i is index 1 then this turns into index 0
+            # It lags behind by 1 each loop 
+            # W here represents the current column of our dp so at the first it would reference row 0 which is all zero columns
+            
+            # print(weights[i-1], "<=", w)
+            
             if weights[i - 1] <= w:
                 # If the condition is met, we calculate the max value
                 # So we've got two options
@@ -70,18 +79,19 @@ def knapsack_dp(weights, values, capacity):
                 #   - Subtract its weight from the current capacity
                 #   - Add its value to the result of that remaining capacity (row above)
                 dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+                
+                # print(dp)
             else:
                 # If it doesn't fit, just carry over the value from the row above (previous item)
                 dp[i][w] = dp[i - 1][w]
 
-    # Return the result stored in the bottom-right corner of the table
     # This represents the max value we can get for the given capacity and all items
     return dp[n][capacity]
 
 
 weights = [1, 3, 4, 5]
 values = [10, 40, 50, 70]
-capacity = 8
+capacity = 4
 
 start_time = time.perf_counter()
 print("\nKnapsack DP Result:", knapsack_dp(weights, values, capacity))  
